9da832d241151dc361780bd6cbf67655b959b439
RamiJammeli <mohamedrami.jammeli@esprit.tn>
2022-07-10 13:29:28 +0200

    design

---
+++ src/components/Back/Dashboard.component.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import Bestcategorie from './statistique/bestcategorie.component';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-6 col-lg-4 col-md-4'>
+                    <Bestcategorie />
+                </div>
+
+                
+                
+            </div>
+
+        );
+    }
+}
